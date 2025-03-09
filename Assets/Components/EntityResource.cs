a6d58e424650c3a4cfe27cb75ff4bd4f344fc688
Lazy Fruit <lazyfruit1@hotmail.com>
2025-03-09 16:54:13 +0300

    Import assets + Set Scene

---
+++ Assets/Components/EntityResource.cs
@@ -0,0 +1,102 @@
+using UnityEngine;
+
+public class EntityResource : MonoBehaviour
+{
+    //variables:
+    private float CurrentValue = 0.0f;
+    public float MaxValue = 10.0f, MinValue = 0.0f;
+    public bool SetMaxValueAtStart = true;
+
+    //delegates:
+    public delegate void ValueIncreased(float CurrentValue, float MaxValue, float DeltaKoef);
+    public event ValueIncreased OnValueIncreased;
+    public delegate void ValueDecreased(float CurrentValue, float MaxValue, float DeltaKoef);
+    public event ValueDecreased OnValueDecreased;
+    public delegate void ValueReachedMinimum();
+    public event ValueReachedMinimum OnValueReachedMinimum;
+
+    //methods:
+    public bool IncreaseValue(float Amount)
+    {
+        bool success = false;
+        //logic goes here
+        if (Amount > 0)
+        {
+            if ((CurrentValue + Amount) > MaxValue)
+            {
+                CurrentValue = MaxValue;
+            }
+            else
+            {
+                CurrentValue = CurrentValue + Amount;
+            }
+            if (OnValueIncreased != null)
+            {
+                OnValueIncreased(CurrentValue, MaxValue, CalculateDeltaKoef());
+                success = true;
+            }
+        }
+        //--------------
+        return success;
+    }
+    public bool DecreaseValue(float Amount)
+    {
+        bool success = false;
+        //logic goes here
+        if (Amount > 0)
+        {
+            if ((CurrentValue - Amount) < MinValue)
+            {
+                CurrentValue = MinValue;
+            }
+            else
+            {
+                CurrentValue = CurrentValue - Amount;
+            }
+            if (OnValueDecreased != null)
+            {
+                OnValueDecreased(CurrentValue, MaxValue, CalculateDeltaKoef());
+                success = true;
+                if (CurrentValue == MinValue)
+                {
+                    if (OnValueReachedMinimum == null)
+                    {
+                        success = false;
+                    }
+                    else
+                    {
+                        OnValueReachedMinimum();
+                    }
+                }
+            }
+        }
+        //--------------
+        return success;
+    }
+    private float CalculateDeltaKoef()
+    {
+        return CurrentValue / MaxValue;
+    }
+    public float GetCurrentValue()
+    {
+        return CurrentValue;
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        if (SetMaxValueAtStart)
+        {
+            IncreaseValue(MaxValue);
+        }
+        else
+        {
+            IncreaseValue(MinValue);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Player/PlayerController.cs
@@ -0,0 +1,98 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    //variables:
+    private HealthPoints HealthPointsComponent;
+    private EnergyPoints EnergyPointsComponent;
+
+    public ParticleSystem[] AttackVFXList, HealVFXList, CounterVFXList, UltimateVFXList;
+
+    //delegates:
+
+    private void HandleOnHealthPointsIncreased(float CurrentValue, float MaxValue, float DeltaKoef) { 
+        //
+    }
+    private void HandleOnHealthPointsDecreased(float CurrentValue, float MaxValue, float DeltaKoef) {
+        //
+    }
+    private void HandleOnHealthPointsReachedMinimum() {
+        //
+    }
+
+    private void HandleOnEnergyPointsIncreased(float CurrentValue, float MaxValue, float DeltaKoef) {
+        //
+    }
+    private void HandleOnEnergyPointsDecreased(float CurrentValue, float MaxValue, float DeltaKoef) {
+        //
+    }
+    private void HandleOnEnergyPointsReachedMinimum() {
+        //
+    }
+    //methods:
+    public bool Attack()
+    {
+        //
+        bool success = false;
+        foreach (ParticleSystem AttackVFX in AttackVFXList)
+        {
+            AttackVFX.Play();
+        }
+        return success;
+    }
+    public bool Heal()
+    {
+        //
+        bool success = false;
+        foreach (ParticleSystem HealVFX in HealVFXList)
+        {
+            HealVFX.Play();
+        }
+        return success;
+    }
+    public bool Counter()
+    {
+        //
+        bool success = false;
+        foreach (ParticleSystem CounterVFX in CounterVFXList)
+        {
+            CounterVFX.Play();
+        }
+        return success;
+    }
+    public bool Ultimate()
+    {
+        //
+        bool success = false;
+        foreach (ParticleSystem UltimateVFX in UltimateVFXList)
+        {
+            UltimateVFX.Play();
+        }
+        return success;
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        HealthPointsComponent = gameObject.GetComponent<HealthPoints>();
+        if (HealthPointsComponent!=null)
+        {
+            HealthPointsComponent.OnValueIncreased += HandleOnHealthPointsIncreased;
+            HealthPointsComponent.OnValueDecreased += HandleOnHealthPointsDecreased;
+            HealthPointsComponent.OnValueReachedMinimum += HandleOnHealthPointsReachedMinimum;
+        }
+        EnergyPointsComponent = gameObject.GetComponent<EnergyPoints>();
+        if (EnergyPointsComponent != null)
+        {
+            EnergyPointsComponent.OnValueIncreased += HandleOnEnergyPointsIncreased;
+            EnergyPointsComponent.OnValueDecreased += HandleOnEnergyPointsDecreased;
+            EnergyPointsComponent.OnValueReachedMinimum += HandleOnEnergyPointsReachedMinimum;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
